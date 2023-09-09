f26f68fc8355b51701d477484883ab119f7dc4cd
Janabahadur <126569465+janbahadur@users.noreply.github.com>
2023-09-09 11:08:32 +0500

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -35,6 +35,6 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', ''))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', ''))
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://janbahadur: janbahadurjan11@filetolink.9dtssgi.mongodb.net/'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', '-1001948565215'))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
